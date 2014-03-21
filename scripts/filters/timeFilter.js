0678a4a81761ca1defa51b69fe7ae27d2fd274c7
Mark Scully <mark.s.scully@gmail.com>
2014-03-21 17:56:03 -0500

    Fix backwards am/pm in timeFilter

--- scripts/filters/timeFilter.js
+++ scripts/filters/timeFilter.js
@@ -1,22 +1,22 @@
 app.filter('timeFilter', function() {
     return function(inputTime) {
-      var twelveHourTime = "";
-      var timeArgs = inputTime.split(':');
-      var inputHours = parseInt(timeArgs[0]);
-      var inputMinutes = timeArgs[1];
-      var timeSuffix = 'AM';
+        var twelveHourTime = "";
+        var timeArgs = inputTime.split(':');
+        var inputHours = parseInt(timeArgs[0]);
+        var inputMinutes = timeArgs[1];
+        var timeSuffix = 'AM';
 
-      if ( inputHours > 12 ){
-          twelveHourTime = inputHours - 12;
-          timeSuffix = 'PM';
-      }
+        if ( inputHours > 11 ){
+            twelveHourTime = inputHours - 12;
+            timeSuffix = 'PM';
+        }
 
-      if ( inputHours === 0) {
-          inputHours = 12;
-      }
-      
-      twelveHourTime = inputHours + ":" + inputMinutes + " " + timeSuffix;
+        if ( inputHours === 0) {
+            inputHours = 12;
+        }
 
-      return twelveHourTime;
+        twelveHourTime = inputHours + ":" + inputMinutes + " " + timeSuffix;
+
+        return twelveHourTime;
     };
-  });
+});
