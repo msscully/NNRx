2755700e104cb49294fbe7528a56eb0856ad3fb1
Mark Scully <mark.s.scully@gmail.com>
2014-05-13 21:06:34 -0500

    Remove console log of emitted events

--- scripts/app.js
+++ scripts/app.js
@@ -28,19 +28,6 @@ app.config([
 }
 ]);
 
-app.config(['$provide', function ($provide) {
-  $provide.decorator('$rootScope', function ($delegate) {
-    var _emit = $delegate.$emit;
-
-    $delegate.$emit = function () {
-      console.log.apply(console, arguments);
-      _emit.apply(this, arguments);
-    };
-
-    return $delegate;
-  });
-}]);
-
 // routing init deferred
 var $routeProviderReference;
 
