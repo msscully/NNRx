24b1904931e99a41715c93b83ae0c3556e5bdf18
Mark Scully <mark.s.scully@gmail.com>
2014-11-19 15:56:10 -0800

    Make notifications auto-cancel

--- scripts/services/reminderStorage.js
+++ scripts/services/reminderStorage.js
@@ -220,7 +220,7 @@ app.factory('reminderStorage', ['CordovaService', 'localNotifications', '$q', fu
       title:      oldReminder.title,
       message:    messageWithScheduledTime,
       date:       newDate,
-      autoCancel: false,
+      autoCancel: true,
       json:       JSON.stringify({ snooze: false}),
       repeat:     'hourly',
     };
@@ -286,7 +286,7 @@ app.factory('reminderStorage', ['CordovaService', 'localNotifications', '$q', fu
           message:      'Use app or lose reminders!',
           date:         newDate,
           repeat:       'minutely',
-          autoCancel:   false,
+          autoCancel:   true,
           json:         JSON.stringify({ snooze: false}),
         };
 
@@ -447,12 +447,14 @@ app.factory('reminderStorage', ['CordovaService', 'localNotifications', '$q', fu
       title:      snoozedNotification.title,
       message:    messageWithScheduledTime,
       date:       fiveMinInFuture,
-      autoCancel: false,
+      autoCancel: true,
       json:       JSON.stringify({ snooze: true}),
       repeat:     'hourly'
     };
 
+    console.log('canceling id: ' + origNotificationId);
     return cancelNotification(origNotificationId, reminderId).then( function() {
+      console.log('adding snooze with id: ' + newNotificationId);
       return localNotifications.add(newNotification).then(function () {
         newNotification.freq = snoozedNotification.freq;
         newNotification.time = snoozedNotification.time;
@@ -482,7 +484,7 @@ app.factory('reminderStorage', ['CordovaService', 'localNotifications', '$q', fu
    *                title: notification.title,
    *                message: notification.message,
    *                date: notification.date,
-   *                autoCancel: false,
+   *                autoCancel: true,
    *                json: JSON.stringify({ snooze: false}),
    *                repeat: repeatInterval,
    *              }],
